d99701038b2de6518c698fe7a8885875c40f6fdd
sbcshop3 <129948335+sbcshop3@users.noreply.github.com>
2025-04-10 10:45:16 +0530

    Update RGBLED_Blink.py

--- Micropython examples/RGBLED_Blink.py
+++ Micropython examples/RGBLED_Blink.py
@@ -2,12 +2,11 @@ from machine import Pin
 from neopixel import NeoPixel
 from time import sleep
 
-'''
-Change based on board
-=> GP45 (Micro S3), GP42 (Micro S2), GP23 (Micro C6), GP10 (Micro C3), GP6 (Micro RP2040)
-'''
-pin = Pin(6, Pin.OUT)   # set GPIO as output to drive NeoPixels
-np = NeoPixel(pin, 1)    # create NeoPixel driver
+RGB_PIN = 6      #  Change as per Board => GP45 (Micro S3), GP42 (Micro S2), GP23 (Micro C6), GP10 (Micro C3), GP6 (Micro RP2040)
+LED_COUNT = 1
+
+pin = Pin(RGB_PIN, Pin.OUT)   # set GPIO as output to drive NeoPixels
+np = NeoPixel(pin, LED_COUNT)    # create NeoPixel driver
 
 np[0] = (255, 255, 255) # set the first pixel to white
 np.write()              # write data to all pixels
