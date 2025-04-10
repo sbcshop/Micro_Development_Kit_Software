fb08136c41b1d6bd9779acbb98674149f9ab9d0f
sbcshop3 <129948335+sbcshop3@users.noreply.github.com>
2025-04-10 12:42:30 +0530

    Update LCD_Demo.py

--- Micropython examples/LCD_Demo.py
+++ Micropython examples/LCD_Demo.py
@@ -8,8 +8,12 @@ import vga1_bold_16x32 as font
 # Initialize SPI interface for TFT screen
 spi = SPI(1, baudrate=40000000, sck=Pin(10), mosi=Pin(11))
 tft = st7789.ST7789(
-        spi, 240, 240, reset=Pin(9, Pin.OUT), cs=Pin(13, Pin.OUT), 
-        dc=Pin(12, Pin.OUT), backlight=Pin(8, Pin.OUT), rotation=1
+        spi, 240, 240, 
+        reset=Pin(9, Pin.OUT), 
+        cs=Pin(13, Pin.OUT), 
+        dc=Pin(12, Pin.OUT), 
+        backlight=Pin(8, Pin.OUT), 
+        rotation=1
     )
 
 time.sleep(0.5)  # Wait for half a second
